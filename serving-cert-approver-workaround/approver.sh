c430eb35683a0138f612d70bb7f07e609fcbe94f
susunola <38539169+susunola@users.noreply.github.com>
2019-12-09 10:47:57 +0700

    Update approver.sh

--- serving-cert-approver-workaround/approver.sh
+++ serving-cert-approver-workaround/approver.sh
@@ -7,14 +7,9 @@ if [ $csrcount -gt 0 ]
 then
   echo "Listing all Pending CSRs..."
   oc get csr | grep Pending
-  echo "Approving only Node Serving Certificate CSR, please approve node-bootstrapper manually..."
 
   for csr in $pendingcsr; do
-    if [[ `oc get csr  -o  custom-columns=:.spec.username $csr` == *"node-bootstrapper"* ]]; then
-  	  echo "Detected $csr as bootstrap request, skipping, please approve manually ..."
-    else
 	  oc adm certificate approve $csr
-    fi
   done  
 else
   echo "No Node Serving Certificate CSR in pending approval state..."
